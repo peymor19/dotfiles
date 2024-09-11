b124764c5aa04a8b35c0c85913ddf352593a0d1a
Peyton Morrison <peyton9148@gmail.com>
2024-09-11 15:53:05 -0400

    telescope search ignore git files
    Signed-off-by: Peyton Morrison <peyton9148@gmail.com>

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,11 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = {".git/", ".cache", "%.o", "%.a", "%.out", "%.class",
+                    "%.pdf", "%.mkv", "%.mp4", "%.zip"}
+            }
+        })
     end
 }
